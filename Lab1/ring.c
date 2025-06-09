de9f22dff6b5668d82ff93667330898ecf70da3e
Mateus Gutemberg <ytubegut@gmail.com>
2025-06-08 23:35:20 -0300

    my third commit: added the time_elapsed and almost finishing the report

--- Lab1/ring.c
+++ Lab1/ring.c
@@ -1,78 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-
+#include <string.h>
 
 MPI_Status status;
 
 void print_helper_send(int* reg_ids, int count, int my_rank, int next_rank){
     printf("rank %d will send the array: [", my_rank);
-    for(int i = 0; i <= count; i++ ){
-        if(i != count)
-            printf("%d, ", reg_ids[i]);
-        else
+    for(int i = 0; i < count; i++ ){
+        if(i == count - 1)
             printf("%d", reg_ids[i]);
+        else
+            printf("%d, ", reg_ids[i]);
     }
-    printf("] to rank %d.\n", next_rank);
+    printf("] located at %p to rank %d\n", reg_ids, next_rank);
 }
 
 void print_helper_receive(int* reg_ids, int count, int my_rank, int prev_rank){
     printf("rank %d received the array: [", my_rank);
-    for(int i = 0; i <= count; i++ ){
-        if(i != count)
-            printf("%d, ", reg_ids[i]);
-        else
+    for(int i = 0; i < count; i++ ){
+        if(i == count - 1)
             printf("%d", reg_ids[i]);
+        else
+            printf("%d, ", reg_ids[i]);
     }
-    printf("] from rank %d\n", prev_rank);
+    printf("] located at %p from rank %d\n", reg_ids, prev_rank);
 
 }
-void helper_rank_even(int* reg_ids, int my_rank, int total_processes, int count, int tam_reg){
-    int next_rank = my_rank + 1;
+void helper_rank_even(int* reg_ids, int* aux_reg, int my_rank, int total_processes, int count, int tam_reg){
+    int next_rank = (my_rank + 1) % total_processes;
     int prev_rank = my_rank - 1;
-    int* aux_reg = malloc(sizeof(int) * tam_reg);
-    if(my_rank == 0){
-        prev_rank = total_processes - 1; // receive from the last process, that is, the third
-        MPI_Send(reg_ids, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
-        print_helper_send(reg_ids, count, my_rank, next_rank);
-        MPI_Recv(aux_reg, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, &status);
-        print_helper_receive(aux_reg, count, my_rank, prev_rank);
-    }else{
-        MPI_Send(reg_ids, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
-        print_helper_send(reg_ids, count, my_rank, next_rank);
-        MPI_Recv(aux_reg, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, &status);
-        print_helper_receive(aux_reg, count, my_rank, prev_rank);
-    }
-    reg_ids[0] = count + 1;//how many elements we have currently in the register
-    reg_ids[count+1] = my_rank;
-    reg_ids = aux_reg;
+    if(my_rank == 0) prev_rank = total_processes - 1; // receive from the last process, that is, the third
+    
+    MPI_Send(reg_ids, count, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+    print_helper_send(reg_ids, count, my_rank, next_rank);
+    MPI_Recv(reg_ids, count, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, &status);
+    print_helper_receive(reg_ids, count, my_rank, prev_rank);
+    reg_ids[0] += 1;//how many elements we have currently in the register
+    reg_ids[count] = my_rank;
 }
 
-void helper_rank_odd(int* reg_ids, int my_rank, int total_processes, int count, int tam_reg){
-    int next_rank = my_rank + 1;
+void helper_rank_odd(int* reg_ids, int* aux_reg, int my_rank, int total_processes, int count, int tam_reg){
+    int next_rank = (my_rank + 1) % total_processes;
     int prev_rank = my_rank - 1;
-    int* aux_reg = malloc(sizeof(int) * tam_reg);
-    if(my_rank == 3){
-        next_rank = 0;
-        MPI_Recv(aux_reg, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, &status);
-        print_helper_receive(reg_ids, count, my_rank, prev_rank);
-        print_helper_send(reg_ids, count, my_rank, next_rank);
-        MPI_Send(reg_ids, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
-    }else{
-        MPI_Recv(aux_reg, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, &status);
-        print_helper_receive(reg_ids, count, my_rank, prev_rank);
-        print_helper_send(reg_ids, count, my_rank, next_rank);
-        MPI_Send(reg_ids, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
-    }
-    aux_reg[0] = count + 1;
-    aux_reg[count+1] = my_rank;
-    reg_ids = aux_reg;
+    
+    MPI_Recv(aux_reg, count, MPI_INT, prev_rank, 0, MPI_COMM_WORLD, &status);
+    print_helper_receive(aux_reg, count, my_rank, prev_rank);
+    print_helper_send(reg_ids, count, my_rank, next_rank);
+    MPI_Send(reg_ids, count, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
+    
+    aux_reg[0] += 1;
+    aux_reg[count] = my_rank;
+    memcpy(reg_ids, aux_reg, tam_reg * sizeof(int));
 }
-void circular_topology(int* reg_ids, int my_rank, int total_processes, int count, int tam_reg){
-    if(my_rank % 2 == 0){//check if the current rank is odd
-        helper_rank_even(reg_ids, my_rank, total_processes, count, tam_reg);
+void circular_topology(int* reg_ids, int* aux_reg, int my_rank, int total_processes, int count, int tam_reg){
+    if(my_rank % 2 == 0){//check if the current rank is even
+        helper_rank_even(reg_ids, aux_reg, my_rank, total_processes, count, tam_reg);
     }else{
-        helper_rank_odd(reg_ids, my_rank, total_processes, count, tam_reg);
+        helper_rank_odd(reg_ids, aux_reg, my_rank, total_processes, count, tam_reg);
     }
 }
 int main(int argc, char* argv[]){
@@ -80,17 +65,26 @@ int main(int argc, char* argv[]){
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &total_processes);
-    //buffer 1 -> len(buffer) 2-> 
-    int count = 1; //how many elements we have currently 
-    int tam_reg = 8;
-    int* reg_ids = calloc(sizeof(int), tam_reg);
-    reg_ids[0] = count;
-    reg_ids[count] = my_rank;
-    for(int i = 0; i < 3; i++){
+    double start = MPI_Wtime();
+    int count = 2; //how many elements we have currently
+    int tam_reg = 8; 
+    int* reg_ids = calloc(tam_reg, sizeof(int));
+    int* aux_reg = calloc(tam_reg, sizeof(int)); 
+    reg_ids[0] = 1;
+    reg_ids[count-1] = my_rank;
+    for(int i = 0; i < 4; i++){
         printf("                          %d ROUND.\n", i+1);
-        circular_topology(reg_ids, my_rank, total_processes, count, tam_reg);
+        printf("--->Im rank %d and my reg_ids is pointing to: %p\n", my_rank, reg_ids);
+        if(my_rank % 2 != 0)
+            printf("--->Im rank %d and my aux_reg is pointing to: %p\n", my_rank, aux_reg);
+            circular_topology(reg_ids, aux_reg, my_rank, total_processes, count, tam_reg);
         count++;
     }
+    double ending = MPI_Wtime();
+    double time_elapsed = ending - start;
+    printf("RANK %d: FINISHED WITH %lf SECONDS.\n", my_rank, time_elapsed);
     MPI_Finalize();
     return 0;
 }
+
+
--- Lab1/tests.c
+++ Lab1/tests.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
-typedef struct RegisterIds{
-    int first_pos;
-    int last_pos;
-    int* ids;
-}RegisterIds;
-MPI_Status status;
+#include <string.h>
 
-int main(int argc, char *argv[]){
-    int a = 1;
-    int b = a + 1;
-    MPI_Init(&argc, &argv);    
-    printf("valor de b: %d valor de a: %d\n", b, a);
-    int* m = malloc(3 * sizeof(int));
-    m[a] = 4;
-    printf("valor de m[a]: %d", m[a]);
-    RegisterIds* register_ids = malloc(sizeof(RegisterIds));
-    MPI_Finalize();
+int* change_pointer(int* a, int* b) {
+    a = b;
+}
+
+void copy_data(int* a, int* b, int size) {
+    memcpy(a, b, size * sizeof(int));  // This actually changes the data `a` points to
+}
+
+int main() {
+    int* original = malloc(3 * sizeof(int));
+    int* new_data = malloc(3 * sizeof(int));
+    int a = 10;
+    int* ptr = &a;
+    int my_arr[3] = {1,2,3};
+    printf("&ptr: %p ptr: %p &a: %p \n", &ptr, ptr, &a);
+    printf("&my_arr: %p my_arr: %p \n", &my_arr, my_arr);
+    
+    original[0] = 1;
+    original[1] = 2;
+    original[2] = 3;
+
+    new_data[0] = 10;
+    new_data[1] = 20;
+    new_data[2] = 30;
+
+    printf("Before pointer change:\n");
+    printf("original: %d, %d, %d\n", original[0], original[1], original[2]);
+
+    change_pointer(original, new_data);  // this will NOT affect original
+
+    printf("After pointer change (no effect):\n");
+    printf("original: %d, %d, %d\n", original[0], original[1], original[2]);
+
+    copy_data(original, new_data, 3);  // this WILL affect original
+
+    printf("After data copy:\n");
+    printf("original: %d, %d, %d\n", original[0], original[1], original[2]);
+
+    free(original);
+    free(new_data);
     return 0;
 }
